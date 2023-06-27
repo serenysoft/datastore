caf036b2922e887d5786a49f3b48902659af9620
Leandro Guindani Gehlen <leandrogehlen@gmail.com>
2023-06-27 16:59:27 -0300

    Manager constructor now supports multiple instances

--- src/replicators/Manager.ts
+++ src/replicators/Manager.ts
@@ -4,6 +4,12 @@ import { Replicator } from './Replicator';
 export class Manager {
   private instances = new Map<RxCollection, Replicator>();
 
+  constructor(instances: Replicator[] = []) {
+    instances.forEach((instance) => {
+      this.add(instance);
+    });
+  }
+
   add(instance: Replicator): this {
     this.instances.set(instance.collection(), instance);
     return this;
