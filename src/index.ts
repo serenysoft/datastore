b1334afb01b01586d22a5a4c1c13f2816a6ee2ae
Leandro Guindani Gehlen <leandrogehlen@gmail.com>
2023-07-06 14:48:59 -0300

    Remove replicators

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,5 @@
 export * from './DataStore';
 export * from './OfflineDataStore';
 
-export * from './replicators/Manager';
-export * from './replicators/OrionReplicator';
-export * from './replicators/Replicator';
-
 export * from './transporters/AxiosTransporter';
 export * from './transporters/Transporter';
