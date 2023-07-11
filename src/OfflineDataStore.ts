53d2dbcf5ee03793130b5eaae2644be9e3aec704
Leandro Guindani Gehlen <leandrogehlen@gmail.com>
2023-07-11 17:24:58 -0300

    Handling ref array properties

--- src/OfflineDataStore.ts
+++ src/OfflineDataStore.ts
@@ -16,7 +16,7 @@ export class OfflineDataStore<T = any> implements DataStore<T> {
 
   async findOne(key: string): Promise<T> {
     const document = await this.options.collection.findOne(key).exec();
-    return document?.deleted ? null : document.toMutableJSON();
+    return document?.deleted ? null : this.populate(document);
   }
 
   async findAll(options?: FindOptions): Promise<T[]> {
@@ -26,21 +26,23 @@ export class OfflineDataStore<T = any> implements DataStore<T> {
     const query = this.options.collection.find(queryOptions);
     const results = await query.exec();
 
-    return results.map((result: RxDocument<T>) => result.toMutableJSON());
+    return Promise.all(results.map((result) => this.populate(result)));
   }
 
   async save(data: T): Promise<any> {
     let result;
-    const keyName = this.key();
-    const keyValue = (data as any)[keyName];
+    const key = this.key();
+    const value = (data as any)[key];
     const collection = this.options.collection;
 
-    if (keyValue) {
-      const document = await collection.findOne(keyValue).exec();
+    data = this.normalize(data);
+
+    if (value) {
+      const document = await collection.findOne(value).exec();
       result = await document.incrementalPatch(data);
     } else {
       result = await collection.insert({
-        [keyName]: uuidv4(),
+        [key]: uuidv4(),
         ...data,
       });
     }
@@ -61,5 +63,46 @@ export class OfflineDataStore<T = any> implements DataStore<T> {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   link(data: Record<string, string | number>): void {
     throw new Error('Method not implemented.');
+  }
+
+  private async populate(document: RxDocument<T>): Promise<T> {
+    const result: any = document.toMutableJSON();
+
+    for (const key in this.collectionReferences()) {
+      const references = await document.populate(key);
+      result[key] = references.map((item: RxDocument<T>) =>
+        item.toMutableJSON()
+      );
+    }
+
+    return result;
+  }
+
+  private normalize(data: any): T {
+    const references = Object.entries(this.collectionReferences());
+    const result = { ...data };
+
+    for (const [key, value] of references) {
+      const schema = this.options.collection.database.collections[value].schema;
+      const primaryPath = schema.primaryPath as string;
+      result[key] = data[key].map((reference: any) => reference[primaryPath]);
+    }
+
+    return result;
+  }
+
+  private collectionReferences(): Record<string, string> {
+    const result: Record<string, string> = {};
+    const entries = Object.entries(
+      this.options.collection.schema.jsonSchema.properties
+    );
+
+    for (const [key, value] of entries) {
+      if (value.type === 'array' && value.ref) {
+        result[key] = value.ref;
+      }
+    }
+
+    return result;
   }
 }
